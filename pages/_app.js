bb217014e489f4d347bfe395440aaf7d3134952e
Pradeep <pmanjunath@gmail.com>
2022-01-23 16:14:59 +0400

    Internationalization

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import React, { useEffect } from 'react';
+import { appWithTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { locale } = useRouter();
+  useEffect(() => {
+    document.dir = locale === 'ar' ? 'rtl' : 'ltr';
+  });
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
