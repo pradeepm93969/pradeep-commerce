df786573ac6dfd12dfb6f22d853153e66860ccdd
Pradeep <pmanjunath@gmail.com>
2022-01-23 22:09:04 +0400

    pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const { i18n } = require('./next-i18next.config');
+const withPwa = require('next-pwa');
 
-module.exports = {
+module.exports = withPwa({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'developement',
+  },
   reactStrictMode: true,
   i18n,
-};
+});
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,69 @@ class MyDocument extends Document {
     return (
       <Html lang="en" dir="ltr">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/img/favicons/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=optional"
+            rel="stylesheet"
+          />
+          {/* Favicon */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/favicons/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/img/favicons/android-192x192.png"
+          />
+          <link
+            href="/img/favicons/android-chrome-192x192.png"
+            sizes="192x192"
+            type="image/png"
+          />
+          <link
+            href="/img/favicons/android-chrome-512x512.png"
+            sizes="512x512"
+            type="image/png"
+          />
+          <link
+            href="/img/favicons/android-chrome-maskable-192x192.png"
+            sizes="192x192"
+            type="image/png"
+            purpose="maskable"
+          />
+          <link
+            href="/img/favicons/android-chrome-maskable-512x512.png"
+            sizes="512x512"
+            type="image/png"
+            purpose="maskable"
+          />
+
+          <link rel="manifest" href="/img/favicons/manifest.json" />
+          {/* PWA primary color */}
+          <meta name="theme-color" content="#121B94" />
+          {/*  Facebook */}
+          <meta property="author" content="Pradeep" />
+          <meta property="og:site_name" content="pradeep-commerce.com" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="pradeep-commerce.com" />
+          <meta property="og:title" content="startup-title" />
+          <meta property="og:description" content="startup-description" />
+          <meta property="og:image" content="/images/favicon.ico" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          {/*  Twitter */}
+          <meta property="twitter:site" content="pradeep-commerce.com" />
+          <meta property="twitter:domain" content="pradeep-commerce.com" />
+          <meta property="twitter:creator" content="Pradeep" />
+          <meta property="twitter:card" content="summary" />
+          <meta property="twitter:image:src" content="/images/favicon.ico" />
+          <meta name="twitter:site" content="pradeep-commerce.com" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content="/images/favicon.ico" />
         </Head>
         <body>
           <Main />
