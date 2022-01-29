1c146db31124e6910df49b0a29e6c0c5fbdd27ab
Pradeep <pmanjunath@gmail.com>
2022-01-29 21:46:06 +0400

    Tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'nprogress/nprogress.css';
 
 import React, { useEffect } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
+
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import { ToastContainer } from 'react-toastify'; //styles of nprogress
+
+//Binding events.
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   const { locale } = useRouter();
@@ -10,7 +22,12 @@ function MyApp({ Component, pageProps }) {
     document.dir = locale === 'ar' ? 'rtl' : 'ltr';
   });
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer limit={4} />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
